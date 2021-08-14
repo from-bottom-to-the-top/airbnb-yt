6863467d8dde32063f8e09db959a58dbcde45753
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2021-08-14 14:29:54 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="red-100">
+      <h2 className="text-pink-200 text-4xl">Rui Reado</h2>
+      <button className="appearance-none bg-red-900 text-white rounded-md p-2">Click on me</button>
+    </div>
+  )
+}
